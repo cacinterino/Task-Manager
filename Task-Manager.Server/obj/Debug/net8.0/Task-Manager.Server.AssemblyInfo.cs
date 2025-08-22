8ab36dfca197a41f3528a4a12e7eabff5041b6d2
cacinterino <LiAndree02@gmail.com>
2025-08-22 13:10:01 +0800

    Added and Fixed

--- Task-Manager.Server/obj/Debug/net8.0/Task-Manager.Server.AssemblyInfo.cs
+++ Task-Manager.Server/obj/Debug/net8.0/Task-Manager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task-Manager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd704adcf3376c2d171b7169e06e659d294096b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cda4d75425bcc4e44406de5873041a354bfdf4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task-Manager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task-Manager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
